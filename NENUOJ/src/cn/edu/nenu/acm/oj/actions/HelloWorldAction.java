fd267e8884ae9209161f26d30af84d2170f86d05
winguse <winguse@winguse-latop>
2013-01-10 23:41:58 +0800

    commit the base framework.
    using a month to lean how to setup the SSH framework, and delete it by
    accident tonight, finally re-setup it. If someone teach me about it, it
    will save me a lot of time.
    
    Signed-off-by: winguse <winguse@winguse-latop>

---
+++ NENUOJ/src/cn/edu/nenu/acm/oj/actions/HelloWorldAction.java
@@ -0,0 +1,38 @@
+package cn.edu.nenu.acm.oj.actions;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+import org.apache.struts2.convention.annotation.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import cn.edu.nenu.acm.oj.entitybeans.User;
+
+import com.opensymphony.xwork2.Action;
+
+@Result(name="success",location="/helloworld-success.jsp")
+public class HelloWorldAction implements Action {
+
+	private String username;
+	@Autowired(required=true)
+	private EntityManagerFactory emf;
+	
+	private String message;
+	
+	@Override
+	public String execute() throws Exception {
+		EntityManager em=emf.createEntityManager();
+		User user=em.find(User.class, 1);
+		message="input username = "+username+", database username:"+user.getUsername();
+		return SUCCESS;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+}
