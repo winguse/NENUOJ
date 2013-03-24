0c16e2f497d537c3c7c17b66a06bc11e984e2e71
tiankong <tiankong@winguse-vm>
2013-03-24 20:31:04 +0800

    add poj crawler

---
+++ NENUOJ/src/cn/edu/nenu/acm/oj/service/impl/remote/POJProblemCrawler.java
@@ -0,0 +1,89 @@
+package cn.edu.nenu.acm.oj.service.impl.remote;
+
+import cn.edu.nenu.acm.oj.eto.CrawlingException;
+import cn.edu.nenu.acm.oj.eto.NetworkException;
+import cn.edu.nenu.acm.oj.eto.RemoteProblemNotFoundException;
+import cn.edu.nenu.acm.oj.service.IProblemCrawler;
+
+public class POJProblemCrawler implements IProblemCrawler {
+
+	@Override
+	public String getJudgerSource() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void crawl(String number) throws NetworkException,
+			CrawlingException, RemoteProblemNotFoundException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getTitle() throws CrawlingException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer getMemoryLimit() throws CrawlingException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Integer getTimeLimit() throws CrawlingException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getDescription() throws CrawlingException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getInput() throws CrawlingException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getOutput() throws CrawlingException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getSampleIn() throws CrawlingException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getSampleOut() throws CrawlingException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getHint() throws CrawlingException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getSource() throws CrawlingException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getOriginalUrl() throws CrawlingException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
